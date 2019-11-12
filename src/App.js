75b88b9ccb168ff3a634290af782a2595c09e438
Greta Solomon <gs2806@nyu.edu>
2019-11-12 21:57:55 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <Profile />   
+//         </header>
+//       </div>
+//     );
+
+// }
+
+// function Profile(props) {
+//   // Declare a new state variable, which we'll call "count"
+  const [activate, setActivate] = useState("");
+
+  function handleClick(e) {
+    let id = e.target.id;
+    setActivate(id);
+  } 
+
+  // if(count > 100) {
+  //   return 
+  // } else {
+    return (
+    <div className="all">
+      
+
+      
+      <button id="firstbutton" className={(activate==="firstbutton") ? "active" : ""} onClick={handleClick}>
+        First Button
+      </button>
+      <button id="secondbutton" className={(activate==="secondbutton") ? "active" : ""} onClick={handleClick}>
+        Second Button
+      </button>
+      <button id="thirdbutton" className={(activate==="thirdbutton") ? "active" : ""} onClick={handleClick}>
+        Third Button
+      </button>
+      {activate}
     </div>
   );
 }
+// }
 
 export default App;
